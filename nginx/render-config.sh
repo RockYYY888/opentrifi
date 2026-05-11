47e7b5c3d39a2deec0ed537cd3c4e584b15fb1c6
RockYYY888 <lywyoyo0318@163.com>
2026-05-11 14:43:54 +0800

    config: support extra nginx hostnames

--- nginx/render-config.sh
+++ nginx/render-config.sh
@@ -2,11 +2,20 @@
 set -eu
 
 DOMAIN="${ASSET_TRACKER_DOMAIN:-_}"
+EXTRA_DOMAINS="${ASSET_TRACKER_EXTRA_DOMAINS:-}"
+SERVER_NAMES="$DOMAIN"
 TEMPLATE="/opt/nginx-templates/default.conf.template"
+
+if [ -n "$EXTRA_DOMAINS" ]; then
+	SERVER_NAMES="$SERVER_NAMES $(printf '%s' "$EXTRA_DOMAINS" | tr ',' ' ')"
+fi
 
 if [ -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/${DOMAIN}/privkey.pem" ]; then
 	TEMPLATE="/opt/nginx-templates/default.conf.ssl.template"
 fi
 
-sed "s/__ASSET_TRACKER_DOMAIN__/${DOMAIN}/g" "$TEMPLATE" \
+sed \
+	-e "s/__ASSET_TRACKER_DOMAIN__/${DOMAIN}/g" \
+	-e "s/__ASSET_TRACKER_SERVER_NAMES__/${SERVER_NAMES}/g" \
+	"$TEMPLATE" \
 	> /etc/nginx/conf.d/default.conf
